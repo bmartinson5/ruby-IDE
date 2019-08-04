a8939c7f5ee3eccf55329c17b101933cf008149c
Benjamin Martinson <benjaminmartinson@iMac-3.local>
2019-08-03 18:56:24 -0700

    initial editor creates with json, tabs now work when pressed

--- src/Editor.js
+++ src/Editor.js
@@ -30,23 +30,67 @@ const compositeDecorator = new Draft.CompositeDecorator([
     component: HashtagSpan,
   }
 ]);
+const createWithHTML = (html) => {
+  const contentBlocks = Draft.convertFromHTML(html);
+  const contentState = Draft.ContentState.createFromBlockArray(contentBlocks);
+  const newEditorState = Draft.EditorState.createWithContent(contentState, compositeDecorator);
+  return newEditorState;
+};
 
-// const firstEditor = createWithHTML("asdf")
+const createWithRawContent = (rawSampleJson) => {
+    const contentState = Draft.convertFromRaw(rawSampleJson);
+    const newEditorState = Draft.EditorState.createWithContent(contentState, compositeDecorator);
+    return newEditorState;
+}
+
+// const firstEditor = createWithHTML("<p>def test_function() </p><p><br/>    <br/><br/><br/>end</p>");
+const firstEditor = createWithRawContent({
+    "entityMap": {},
+    "blocks": [
+        {
+            "key": "5h45l",
+            "text": "def test()",
+            "type": "unstyled",
+            "depth": 0,
+            "entityRanges": [],
+            "data": {}
+        },
+        {
+            "key": "5h45r",
+            "text": "   Your code here...",
+            "type": "unstyled",
+            "depth": 0,
+            "entityRanges": [],
+            "data": {}
+        },
+        {
+            "key": "5h45x",
+            "text": "",
+            "type": "unstyled",
+            "depth": 0,
+            "entityRanges": [],
+            "data": {}
+        },
+        {
+            "key": "5h45t",
+            "text": "end",
+            "type": "unstyled",
+            "depth": 0,
+            "entityRanges": [],
+            "data": {}
+        }
+    ]
+});
 
 export default class HashtagDecorator extends React.Component {
   state = {
-    editorState: Draft.EditorState.createEmpty(compositeDecorator),
+    // editorState: Draft.EditorState.createEmpty(compositeDecorator),
+    editorState: firstEditor,
+    lineNums: 4,
     text: "",
   };
 
 
-  createWithHTML = (html) => {
-    console.log('here');
-    const contentBlocks = Draft.convertFromHTML(html);
-    const contentState = Draft.ContentState.createFromBlockArray(contentBlocks);
-    const newEditorState = Draft.EditorState.createWithContent(contentState);
-    this.setState({ editorState: newEditorState });
-  };
 
  setLineNums = () => {
    const lineNums = this.state.editorState.getCurrentContent().getBlocksAsArray().length
@@ -68,18 +112,26 @@ export default class HashtagDecorator extends React.Component {
     });
   }
 
+  handleTab = (e) => {
+    e.preventDefault();
+
+    let currentState = this.state.editorState;
+    let newContentState = Draft.Modifier.replaceText(
+      currentState.getCurrentContent(),
+      currentState.getSelection(),
+      "    "
+    );
+
+    this.setState({
+      editorState: Draft.EditorState.push(currentState, newContentState, 'insert-characters')
+    });
+  }
+
 
 
   render() {
-    console.log(this.state.text);
-    if(this.state.text === ""){
-      console.log('here');
-      this.createWithHTML("def test_function() <br/>  Your code here...<br/><br/><br/>end")
-      this.setState({text: "e"})
-    }
-
     const lineNumsOutput = [];
-    for(let i = 1; i <= this.state.lineNums+2; ++i){
+    for(let i = 1; i <= this.state.lineNums; ++i){
       lineNumsOutput.push(<div className="line-number" key={i.toString()}>{i.toString()}</div>);
     }
 
@@ -92,6 +144,7 @@ export default class HashtagDecorator extends React.Component {
           <Draft.Editor
             editorState={this.state.editorState}
             onChange={this.editorStateChanged}
+            onTab={this.handleTab}
             />
         </div>
       </div>
