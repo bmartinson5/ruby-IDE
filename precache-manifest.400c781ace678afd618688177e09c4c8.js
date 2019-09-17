bb41dc9de923bde76e9dfa96d8cbf19234b97729
Student <example@example.com>
2019-09-17 10:09:22 -0700

    Updates

---
+++ precache-manifest.400c781ace678afd618688177e09c4c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aefb2cac77e2ff6ed30fbaadc7d16c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a55cbf0ac8ab869cfcb2",
+    "url": "/static/css/main.27295e9f.chunk.css"
+  },
+  {
+    "revision": "34cc1635fb4b27513afe",
+    "url": "/static/js/2.8e92d66d.chunk.js"
+  },
+  {
+    "revision": "a55cbf0ac8ab869cfcb2",
+    "url": "/static/js/main.86afbf9f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
