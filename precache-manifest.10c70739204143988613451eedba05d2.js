678e1f7d51c3f1b3923cbd4b1d78bd4b7056561b
Student <example@example.com>
2019-09-23 10:48:28 -0700

    Updates

---
+++ precache-manifest.10c70739204143988613451eedba05d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc01e32d36c86fd422e34b808b07606",
+    "url": "/ruby-runner/index.html"
+  },
+  {
+    "revision": "9076bbbc9c4489279347",
+    "url": "/ruby-runner/static/css/main.032badda.chunk.css"
+  },
+  {
+    "revision": "4e921d965e5da7fd394d",
+    "url": "/ruby-runner/static/js/2.e5b7ab5a.chunk.js"
+  },
+  {
+    "revision": "9076bbbc9c4489279347",
+    "url": "/ruby-runner/static/js/main.de38cc4e.chunk.js"
+  },
+  {
+    "revision": "6c2dfa95c481316490e2",
+    "url": "/ruby-runner/static/js/runtime~main.f8cdb8fd.js"
+  }
+]);
