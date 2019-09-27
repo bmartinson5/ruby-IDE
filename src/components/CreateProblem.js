ec7572d5072a72feaa87082933ea12c92f1f9172
Benjamin Martinson <Ben@Benjamins-MacBook-Pro-3.local>
2019-09-27 14:35:03 -0700

    form can function with multiple test inputs

--- src/components/CreateProblem.js
+++ src/components/CreateProblem.js
@@ -11,9 +11,10 @@ export default class CreateProblem extends Component {
       descriptionContent: "",
       descriptionExample: "",
       params: "",
-      testInputs: "",
-      testDescriptions: "",
-      expectedOutputs: "",
+      testInputs: [],
+      testDescriptions: [],
+      expectedOutputs: [],
+      numberOfTests: 1
     }
   }
 
@@ -21,18 +22,42 @@ export default class CreateProblem extends Component {
     this.setState({[evt.target.name]: evt.target.value });
   }
 
+  handleTestChange = (evt, testNumber) => {
+    const type = this.state[evt.target.name].slice();
+    type[testNumber] = evt.target.value;
+    console.log('type', type[testNumber]);
+    this.setState({[evt.target.name]: type})
+  }
+
+  reformatProblemObject = () => {
+
+    this.props.addProblem(this.state)
+  }
+
   render(){
+
+    const tests = []
+    for(let i = 0; i < this.state.numberOfTests; ++i){
+      tests.push(
+        <div className="test">
+          Test {i+1}.<br/>
+        <input type="text" value={this.state.testDescriptions[i]} name="testDescriptions" onChange={(e) => this.handleTestChange(e, i)} placeholder="Test Descriptions"/><br/>
+          <input type="text" value={this.state.testInputs[i]} name="testInputs" onChange={(e) => this.handleTestChange(e, i)} placeholder="Test Inputs"/><br/>
+          <input type="text" value={this.state.expectedOutputs[i]} name="expectedOutputs" onChange={(e) => this.handleTestChange(e, i)} placeholder="Expected Outputs"/><br/>
+
+        </div>
+      )
+
+    }
     return (
       <div>Create a custom problem to Solve! <br/>
         <input type="number" value={this.state.functionName} name="functionName" onChange={this.handleChange} placeholder="Name of the main function called"/><br/>
         <input type="text" value={this.state.name} name="name" onChange={this.handleChange} placeholder="Problem Name"/><br/>
         <input type="text" value={this.state.descriptionContent} name="descriptionContent" onChange={this.handleChange} placeholder="Description Content"/><br/>
         <input type="text" value={this.state.descriptionExample} name="descriptionExample" onChange={this.handleChange} placeholder="Description Example"/><br/>
-        <input type="text" value={this.state.params} name="params" onChange={this.handleChange} placeholder="Function Parameters"/><br/>
-        <input type="text" value={this.state.testDescription} name="testDescription" onChange={this.handleChange} placeholder="Test Descriptions"/><br/>
-        <input type="text" value={this.state.testInputs} name="testInputs" onChange={this.handleChange} placeholder="Test Inputs"/><br/>
-        <input type="text" value={this.state.expectedOutputs} name="expectedOutputs" onChange={this.handleChange} placeholder="Expected Outputs"/><br/>
-        <button onClick={() => this.props.addProblem(this.state)}>Add Problem</button><br/>
+        {tests}
+
+        <button onClick={() => this.reformatProblemObject()}>Add Problem</button><br/>
       </div>
     );
   }
